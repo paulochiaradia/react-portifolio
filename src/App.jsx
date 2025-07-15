432bfce93bf8f008cc7a488efb2f0af90ac7b012
Paulo Roberto Chiardia Neto <97291302+paulochiaradia@users.noreply.github.com>
2025-07-15 10:56:03 -0300

    feat: add About component with content and styling for personal introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import style from './App.module.css'
 import { Navbar } from './componets/navbar/navbar'
 import {Hero} from './componets/hero/hero'
+import { About } from './componets/about/about'
+
 
 function App() {
   return <div className={style.App}>
-    <Navbar />
-    <Hero />
+    <Navbar/>
+    <Hero/>
+    <About/>
   </div>
 }
 
