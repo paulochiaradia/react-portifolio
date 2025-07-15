0da4822480d76b5b4848ea66419726edfb59fc0a
Paulo Chiaradia <paulo.chiaradia@outlook.com>
2025-07-14 22:22:16 -0300

    feat: add Hero component with styling and image integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import style from './App.module.css'
 import { Navbar } from './componets/navbar/navbar'
+import {Hero} from './componets/hero/hero'
 
 function App() {
   return <div className={style.App}>
     <Navbar />
+    <Hero />
   </div>
 }
 
---
+++ src/componets/hero/hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./hero.module.css";
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Olá, eu sou Paulo Chiaradia</h1>
+                <p className={styles.description}>Possuo forte interesse por microcontroladores e análise de dados, sendo movido pela curiosidade em explorar novas tecnologias e soluções inovadoras.</p>    
+                <a href="mailto:eu@paulochiaradia.com.br" className={styles.contactBtn}>Entre em Contato</a>
+            </div>
+            <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg}/>
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+        </section>
+    );
+}
